53e3e922c9dd33ed1887d9469107f9d6fbeaeffa
Harry Marah <info@harrymarah.com>
2022-01-07 12:14:27 +0000

    Added logic to booking route to add a booking to a desk number in an office.

--- routes/booking.js
+++ routes/booking.js
@@ -2,14 +2,23 @@ const express = require('express')
 const router = express.Router({mergeParams: true})
 
 const Booking = require('../models/booking')
+const Office = require('../models/office')
 
-router.post('/', (req, res) => {
-    const {deskIndex, date, amBooking, pmBooking, allDayBooking} = req.body.booking
-    res.send(req.body.booking)
-    console.log(deskIndex)
+router.post('/', async (req, res) => {
+    const booking = new Booking(req.body.booking)
+    const office =  await Office.findById(req.params.officeid)
+    const deskNumber = office.desks[req.body.booking.deskIndex]
+
+    if(req.body.booking.bookedAM === 'on') booking.bookedAM = true
+    if(req.body.booking.bookedPM === 'on') booking.bookedPM = true
+
+    deskNumber.push(booking)
+    await deskNumber.save()
+
+    console.log(booking)
+    res.send(deskNumber)
 })
 
 module.exports = router;
-
 
 
